4b1cb34fe44b43535d94ea215314e7154ad6818f
Bouchat Marie-Ange <bouchat.marieange@gmail.com>
2017-10-18 08:54:19 +0200

    transfert fichier app.js bac à sable sur app.js du hackaton cookies sur branche marieange

--- app.js
+++ app.js
@@ -5,14 +5,27 @@ var multiplicateurTombe=1;
 
 var cookieElement;
 
-var cookieElement=document.getElementById("cookie");
-cookieElement.onclick=function () {
+var cookieElement = document.getElementById("cookie");
+
+// Fonction qui ajoute 1 pt à chaque click * les multiplicateur ajouté par chaque objet acheté
+cookieElement.onclick=function ()
+{
   scoreTotal=scoreTotal+(1*multiplicateurClick);
   console.log(scoreTotal);
 }
 
-setInterval(function () {
-  scoreTotal=scoreTotal+(1*multiplicateurTombe);
-},1000)
+Explication de la fonction onclick
 
-// Création de la liste des objets bonus
+
+//Fonction add 1pt / sec * multi tombe ajouté avec objets achetés
+setInterval(function ()
+{
+  scoreTotal=scoreTotal+(1*multiplicateurTombe);
+  // console.log(scoreTotal);
+},1000);
+
+// Explication de la methode setInterval
+// setInterval est une methode qui appelle une fonction qui va s'exécuter tout les x temps.
+// Le temps d'interval entre deux exécution de la fonction contenue dans setInterval est définie en milliseconde (ms),
+// juste après l'accolade de la fonction, on met une virgule et le temps en ms (1 seconde = 1000 ms) et est suivie d'une parenthèse puis d'un point virgule
+// Dans le cas présent la fonction va stocké dans la variable scoreTotal, somme cookies total + nouveaux cookies ajoutés/sec en fonction des objets achetés (5cookies/seconde etc...)
